bc7ed30b141869c10584fc8645175b6b9a72361d
Zyad Saber <zyad.2002.best@gmai.com>
2024-02-02 17:32:33 +0200

    react-router-v6

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About";
+import Vans from "./pages/Vans";
+import "./server"
+
+const App =  ()=>{
+
+    return(
+        <BrowserRouter>
+        <header>
+          <Link className="site-logo" to="/">#VanLife</Link>
+          <nav>
+            <Link to="/about">About</Link>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<Vans />} />
+        </Routes>
+      </BrowserRouter>
+    )
+}
+
+export default App
